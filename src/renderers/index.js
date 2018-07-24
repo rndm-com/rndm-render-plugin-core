c62e46331a778043750dff0665d649d1579da9e3
Paul Napier <>
2018-07-24 18:16:27 +1000

    Split core renderer to own file. Add function chain renderer

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,35 +1,14 @@
-import React from 'react';
-import { View } from 'react-native';
-import { get } from 'lodash';
-import { components, resolve, render } from 'rndm-render';
-
-const Default = View;
-
-const core = ({ type, props: { children, middleware = [], ...props } = {} } = {}) => {
-  try {
-    const Item = get(components, type, Default);
-
-    const Element = (properties) => (
-      typeof Item === 'function' &&
-      <Item {...props} {...properties}>
-        {render(children)}
-      </Item>
-    );
-
-    const Resolved = resolve(middleware)(Element);
-
-    return (
-      <Resolved />
-    );
-  } catch (e) {
-    return null
-  }
-};
+import core from './core';
+import functionChain from './functionChain';
 
 const renderers = [
   {
     type: 'core',
     value: core,
+  },
+  {
+    type: 'functionChain',
+    value: functionChain,
   },
 ];
 
