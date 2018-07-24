56655706868824817e9e860849ebac5355698a26
Paul Napier <>
2018-07-24 18:15:56 +1000

    Add pure Renderer component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,14 @@
 import Logo from './Logo';
+import Renderer from './Renderer';
 
 const components = [
   {
     type: 'Logo',
     value: Logo,
+  },
+  {
+    type: 'Renderer',
+    value: Renderer,
   },
 ];
 
