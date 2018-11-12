280ca904c8eccd76b70dd3010378f04414f1a394
Paul Napier <>
2018-11-12 17:53:21 +1100

    Improved rendering

--- src/renderers/functionChain.js
+++ src/renderers/functionChain.js
@@ -1,19 +1,24 @@
 import { noop, get } from 'lodash';
 import { methods } from '@rndm/render';
 
-const functionChain = (input) => {
+const functionChain = (input, additional) => {
   if (typeof input === 'function') return input;
   if (Array.isArray(input)) return input.map(functionChain);
+  if (typeof input === 'string' && input.startsWith('$.')) {
+    const [ option, or ] = input.split('||');
+    const getter = option.split('.').slice(1).join('.');
+    return get(additional, getter, or);
+  }
   if (typeof input !== 'object' || !input.isFunc) return input;
   const { type, args, execute} = input;
   const method = () => {
     try {
-      return get(methods, type, noop)(...args.map(functionChain));
+      return get(methods, type, noop)(...args.map(arg => functionChain(arg, additional)));
     } catch (_) {
       return noop();
     }
   };
-  return Array.isArray(execute) ? method(...execute.map(functionChain)) : method;
+  return Array.isArray(execute) ? method(...execute.map(arg => functionChain(arg, additional))) : method;
 };
 
 export default functionChain;
