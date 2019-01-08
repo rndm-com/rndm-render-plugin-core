4f5a0732dcd1f49c130970322b738f4b973aa3dd
Paul Napier <>
2019-01-08 12:23:45 +1100

    Add renderItem method for FlatList and SectionList rendering

--- src/methods/index.js
+++ src/methods/index.js
@@ -2,8 +2,13 @@ import generate from './utils/generate';
 import concatenate from './concatenate';
 import or from './or';
 import and from './and';
+import renderItem from './renderItem';
 
 const methods = [
+  {
+    type: 'renderItem',
+    value: renderItem,
+  },
   {
     type: 'Number.parseFloat',
     value: (input, defaultValue = Number.NaN) => Number.parseFloat(input) || defaultValue,
